a7fbb0cfd1216532b8defb99fe114535b58fb3f6
Maxiikim <max97kim@gmail.com>
2025-02-19 15:35:14 -0800

    Finished excercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let word = ''
+    for(i = str.length; i > 0; i--){
+        word = word + str.at(i-1)
+    }
+    return word
 };
 
 // Do not edit below this line
