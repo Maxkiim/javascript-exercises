e79f783dc2f71183246f5e44953e47cda33f3c85
Maxiikim <max97kim@gmail.com>
2025-02-20 13:14:48 -0800

    exercise 06 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (((year % 100 == 0) && //check if year is divisible by 100  
+        (year % 400 !== 0)) || // check if year is divisible by 400
+        // if year is divisible by 100 but not 400 return false
+        (year % 4 !== 0) // Check if year is divisible by 4
+        // if year is not divisible by 4 return false
+        ){
+            return false
+    } else {
+        return true
+    }
 
 };
 
