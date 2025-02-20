4b3ddf68a751832548f0e15462472df946d4d73d
Maxiikim <max97kim@gmail.com>
2025-02-20 13:00:13 -0800

    finished exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let lowerNum = first;
+    let higherNum = second;
+    let num = 0
+    switch(true){
+        case (typeof first !== 'number' || typeof second !== 'number'):
+            return 'ERROR';
+            break;
+        case (!Number.isInteger(first) || !Number.isInteger(second)):
+            return 'ERROR';
+            break;
+        case (first < 0 || second < 0):
+            return 'ERROR';
+            break;
+        case (first > second):
+            lowerNum = second;
+            higherNum = first;
+    }
 
+    for(i = lowerNum; i <= higherNum; i++){
+        num += i
+    }
+    return num
 };
 
 // Do not edit below this line
