9611d3f56c6975eeb9462e03e543faf0a356b2aa
Maxiikim <max97kim@gmail.com>
2025-02-20 13:43:25 -0800

    Exercise 07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let converted = (num - 32) * 5/9
+  if (!Number.isInteger(converted)){
+    converted = converted.toFixed(1)
+  }
+  return Number(converted)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let converted = (num*9/5)+32
+  if (!Number.isInteger(converted)){
+    converted = converted.toFixed(1)
+  }
+  return Number(converted)
 };
 
 // Do not edit below this line
