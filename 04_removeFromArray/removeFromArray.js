ee206b3f3c4db0d352f10bc74953bfc583eaafc7
Maxiikim <max97kim@gmail.com>
2025-02-20 11:09:41 -0800

    Finished exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    // create new array to return
+    const newArray = []
+    array.forEach((element) => {
+        if(!args.includes(element)){
+            newArray.push(element);
+        }
+    });
+    return newArray
 };
 
 // Do not edit below this line
